647a2753a8f91b2457d9e66a3b990b422942202a
Keith Klein <keithk@microsoft.com>
2026-02-18 11:46:17 -0800

    Bump version to 2.0.1 (versionCode 7)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.TeraFlop.InsulinCalc"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "2.0.0"
+        versionCode = 7
+        versionName = "2.0.1"
     }
 
     signingConfigs {
